129cecf4d41269cc781d1233d0990b5c1c7b32d5
TauruS111 <134150044+TauruS111@users.noreply.github.com>
2023-10-08 09:56:08 +0000

    Deploying to gh-pages from @ TauruS111/hometask-8-js@100966618fb9f544d2abd1006f461f0b321c207e 🚀

--- 02-video.5cff39fd.js
+++
@@ -1,2 +0,0 @@
-const e=new Player("vimeo-player"),t=localStorage.getItem("videoplayer-current-time");t&&e.setCurrentTime(t),e.on("timeupdate",throttle((e=>{localStorage.setItem("videoplayer-current-time",e.seconds)}),1e3));e.on("play",(function(e){}));
-//# sourceMappingURL=02-video.5cff39fd.js.map
--- 02-video.63af4d0c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=new Player("vimeo-player"),t="videoplayer-current-time",o=localStorage.getItem(t);o&&e.setCurrentTime(o),e.on("timeupdate",throttle((function(e){localStorage.setItem(t,e.seconds)}),1e3));e.on("play",(function(e){}))}();
-//# sourceMappingURL=02-video.63af4d0c.js.map
